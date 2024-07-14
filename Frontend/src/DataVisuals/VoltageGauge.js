de9794c298710982c7c5287e71fd0052cfed463e
Parsa <mo.parsa@yahoo.com>
2024-07-15 03:47:39 +1000

    Add ampmeter

--- Frontend/src/DataVisuals/VoltageGauge.js
+++ Frontend/src/DataVisuals/VoltageGauge.js
@@ -1,20 +1,30 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Img, Div, Text, Container, Span } from "../Styles/VoltageGaugeStyle";
+import React, { useEffect, useState } from "react";
+import {
+  Img,
+  Div,
+  AmpText,
+  VoltText,
+  Container,
+} from "../Styles/VoltageGaugeStyle";
 import Voltmeter from "../Images/Voltmeter.png";
+import Ampmeter from "../Images/Ampmeter.png";
 
 const VoltageGauge = ({ voltage = 0 }) => {
-  const [Volt, setVolt] = useState(0);
+  const [Volt, setVolt] = useState(voltage);
   useEffect(() => {
     setVolt(voltage);
   }, [voltage]);
+
   return (
     <Div>
       <Container>
-        <Img src={Voltmeter} alt="Description" />
-        <Text>
+        <Img src={Voltmeter} alt="voltmeter" />
+        <Img src={Ampmeter} alt="ampmeter" />
+        <VoltText>
           {Volt}
           {/*<Span>.0</Span>*/}
-        </Text>
+        </VoltText>
+        <AmpText>40</AmpText>
       </Container>
     </Div>
   );
--- Frontend/src/Styles/VoltageGaugeStyle.js
+++ Frontend/src/Styles/VoltageGaugeStyle.js
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 
 const Img = styled.img`
-  width: 500px;
+  width: 400px;
 `;
 
 const Container = styled.div`
@@ -9,18 +9,27 @@ const Container = styled.div`
   display: inline-block;
 `;
 
-const Text = styled.span`
+const VoltText = styled.span`
   position: absolute;
   top: 50%;
-  left: 50%;
+  left: 25%;
   transform: translate(-50%, -50%);
-  color: #000000; // or any color that makes the text visible on the image
-  font-size: 8rem;
+  color: #00aeff; // or any color that makes the text visible on the image
+  font-size: 7rem;
+  font-family: "DSDIGI";
+`;
+const Span = styled.span`
+  font-size: 5.5rem;
   font-family: "DSDIGI";
 `;
 
-const Span = styled.span`
-  font-size: 6rem;
+const AmpText = styled.span`
+  position: absolute;
+  top: 50%;
+  left: 75%;
+  transform: translate(-50%, -50%);
+  color: #ff0000; // or any color that makes the text visible on the image
+  font-size: 7rem;
   font-family: "DSDIGI";
 `;
 
@@ -30,4 +39,4 @@ const Div = styled.div`
   align-items: center; // Optional: To center the container vertically in the viewport
 `;
 
-export { Img, Div, Text, Container, Span };
+export { Img, Div, AmpText, VoltText, Container, Span };
